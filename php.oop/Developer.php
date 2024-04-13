1698d8b85e02ad27240cc7eadde622b83d044442
MarinaMoskvina00 <166218651+MarinaMoskvina00@users.noreply.github.com>
2024-04-13 19:14:03 +0500

    oop
    oop

---
+++ php.oop/Developer.php
@@ -0,0 +1,47 @@
+<?php
+
+class Developer
+{
+    public $name;
+    public $age;
+    public $post;
+
+    public function value()
+    {
+        echo 'evaluate the task';
+    }
+}
+
+class PhpDevelper extends Developer
+{
+
+    public function __con
+    public function value()
+    {
+        echo 'evaluate the task';
+    }
+}
+
+class JavaDevelper extends Developer
+{
+    public function value()
+    {
+        echo 'evaluate the task';
+    }
+}
+
+class PythonDevelper extends Developer
+{
+    public function value()
+    {
+        echo 'evaluate the task';
+    }
+}
+
+class RubyDevelper extends Developer
+{
+    public function value()
+    {
+        echo 'evaluate the task';
+    }
+}
